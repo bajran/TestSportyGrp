9fc48380ce4d32a086c934b5d94d28ca48289bed
Akshay Biyani <akshay@openenvoy.com>
2025-06-16 12:08:20 +0530

    Move deploy.yml to root

--- sporttest/vite.config.ts
+++ sporttest/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TestSportyGrp/sporttest/", // Updated to match repository and subdirectory structure
+  base: "/TestSportyGrp/",
 });
